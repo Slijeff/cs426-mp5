530dcca61bb2f28be0490c10dd5b284ce72bf766
spring1627 <54809786+spring1627@users.noreply.github.com>
2023-12-06 23:37:58 -0600

    UnitSCCP.cpp Statistics are disabled.  Build with asserts or with -DLLVM_FORCE_ENABLE_STATS

--- UnitSCCP.cpp
+++ UnitSCCP.cpp
@@ -1,10 +1,15 @@
 // Usage: opt -load-pass-plugin=libUnitProject.so -passes="unit-sccp"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "UnitSCCP.h"
 
-#define DEBUG_TYPE UnitSCCP
+//#define DEBUG_TYPE UnitSCCP
+#define DEBUG_TYPE "UnitSCCP"
 // Define any statistics here
+STATISTIC(NumInstRemoved, "Number of instructions removed");
+STATISTIC(NumDeadBlocks, "Number of basic blocks unreachable");
+STATISTIC(NumInstReplaced, "Number of instructions replaced with (simpler) instructions");
 
 using namespace llvm;
 using namespace cs426;
