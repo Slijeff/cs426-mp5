1b130137f7fb315a95c9b5f5e4b3a664df2edbdd
floraz2 <floraz2@fa23-cs426-034.cs.illinois.edu>
2023-12-13 14:31:33 -0600

    UnitLICM checkInstructionOpcode

--- UnitLICM.cpp
+++ UnitLICM.cpp
@@ -3,7 +3,7 @@
 #include "llvm/Support/raw_ostream.h"
 
 #include "UnitLICM.h"
-#include "UnitLoopInfo.h"
+// #include "UnitLoopInfo.h"
 
 #define DEBUG_TYPE UnitLICM
 // Define any statistics here
@@ -17,9 +17,44 @@ PreservedAnalyses UnitLICM::run(Function& F, FunctionAnalysisManager& FAM) {
   // Acquires the UnitLoopInfo object constructed by your Loop Identification
   // (LoopAnalysis) pass
   UnitLoopInfo &Loops = FAM.getResult<UnitLoopAnalysis>(F);
+
   AAResults &AA = FAM.getResult<AAManager>(F);
   // Perform the optimization
+  /*
+  for (auto &I: instructions(F)) {
+    dbgs() << checkIsHandled(I) << '\n';
+  }*/
+  
+  hoist(Loops);
 
   // Set proper preserved analyses
   return PreservedAnalyses::all();
+}
+
+bool UnitLICM::checkIsHandled(Instruction &I) {
+  bool isHandled = false;
+  unsigned int opcode = I.getOpcode();
+  if (I.isUnaryOp()) {
+    isHandled = true;
+  } else if (I.isBinaryOp()) {
+    isHandled = true;
+  } else if (I.isShift() || I.isBitwiseLogicOp()) {
+    isHandled = true;
+  } else if (opcode == Instruction::BitCast) {
+    isHandled = true;
+  } else if (opcode == Instruction::ICmp) {
+    isHandled = true;
+  } else if (opcode == Instruction::FCmp) {
+    isHandled = true;
+  } else if (opcode == Instruction::Select) {
+    isHandled = true;
+  } else if (opcode == Instruction::GetElementPtr) {
+    isHandled = true;
+  } else if (opcode == Instruction::Load) {
+    isHandled = true;
+  } else if (opcode == Instruction::Store) {
+    isHandled = true;
+  }
+
+  return isHandled;
 }
--- UnitLICM.h
+++ UnitLICM.h
@@ -3,13 +3,17 @@
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/PassManager.h"
 
+#include "UnitLoopInfo.h"
+
 using namespace llvm;
 
 namespace cs426 {
 /// Loop Invariant Code Motion Optimization Pass
 struct UnitLICM : PassInfoMixin<UnitLICM> {
   PreservedAnalyses run(Function& F, FunctionAnalysisManager& FAM);
-
+  
+  bool checkIsHandled(Instruction &I);
+  void hoist(UnitLoopInfo &Loops);
 public:
   int NumHoistedStores;
   int NumHoistedLoads;
